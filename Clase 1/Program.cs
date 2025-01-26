91676cb8be73a7a60b15a832a2c7c46bef670be4
Var-Guz <davidvarguz.7@unitec.edu>
2025-01-25 21:26:41 -0600

    adding ave data

--- Clase 1/Program.cs
+++ Clase 1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Drawing;
+using Clase_1;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Ave ave = new Ave();
+ave.Nombre = "Loro";
+ave.Especie = "Amazona";
+ave.Color = "Verde";
+ave.Tamano = 30.5;
+ave.Habitat = "Bosques tropicales";
+ave.Imprimir();
+
