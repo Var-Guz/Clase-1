41a79528b273da71584a2ba23d77e7d1b6147ec3
Var-Guz <davidvarguz.7@unitec.edu>
2025-01-26 00:08:27 -0600

    data

--- Clase 1/Program.cs
+++ Clase 1/Program.cs
@@ -14,3 +14,13 @@ ave.Familia.NumeroEspecie = "393";
 ave.Familia.Caracteristicas = "aves coloridas";
 ave.Imprimir();
 
+Ave ave2 = new Ave();
+ave2.Nombre = "Flamenco";
+ave2.Especie = "roseus";
+ave2.Color = "rosa";
+ave2.Tamano = 120;
+ave2.Habitat = "lagunas salinas";
+ave2.Familia.Nombre = "copteridae";
+ave2.Familia.NumeroEspecie = "6";
+ave2.Familia.Caracteristicas = "aves zancuadas con patas y coellos largos";
+ave2.Imprimir();
