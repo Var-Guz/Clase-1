8d7fc36f9172364bc1e6bc08403e6c68ba236d46
Var-Guz <davidvarguz.7@unitec.edu>
2025-01-25 23:36:32 -0600

    adding familiaave class

---
+++ Clase 1/FamiliaAve.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Clase_1
+{
+    public class FamiliaAve
+    {
+        public string? Nombre { get; set; }
+        public string? NumeroEspecie { get; set; }
+        public string? Caracteristicas { get; set; }
+
+
+    }
+}
